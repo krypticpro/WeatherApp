37fa0f85722886b2a7cedabbdb5ddad2a95a52e4
kryptic <krypticreal@gmail.com>
2025-08-31 13:43:21 -0600

    Update Icons
    - Add Alt Text
    - Add New Icons

--- main.js
+++ main.js
@@ -19,12 +19,16 @@ function getWeatherData(URL) {
       let iconId = data.weather[0].icon;
       if (iconId === "01d" || iconId === "01n") {
         icon.src = "./assets/weathericons/sun.png";
+        icon.alt = "sun";
       } else if (iconId === "02d" || iconId === "02n") {
         icon.src = "./assets/weathericons/cloudy-day.png";
+        icon.alt = "sunny cloud";
       } else if (iconId === "03d" || iconId === "03n") {
         icon.src = "./assets/weathericons/cloud.png";
+        icon.alt = "cloud";
       } else if (iconId === "04d" || iconId === "04n") {
         icon.src = "./assets/weathericons/cloudy.png";
+        icon.alt = "broken cloud";
       } else if (
         iconId === "09d" ||
         iconId === "09d" ||
@@ -32,8 +36,18 @@ function getWeatherData(URL) {
         iconId === "10n"
       ) {
         icon.src = "./assets/weathericons/rainy.png";
+        icon.alt = "rain";
+      } else if (iconId === "11d" || iconId === "11n") {
+        icon.src = "./assets/weathericons/thunderstorm.png";
+        icon.alt = "thunderstorm";
+      } else if (iconId === "13d" || iconId === "13n") {
+        icon.src = "./assets/weathericons/snow.png";
+        icon.alt = "snowflake";
+      } else if (iconId === "50d" || iconId === "50n") {
+        icon.src = "./assets/weathericons/mist.png";
+        icon.alt = "windy/mist";
       } else {
-        icon.alt = "idk";
+        icon.alt = "We currently don't have the image you're looking for.";
       }
       temp.innerHTML = `${data.main.temp}&deg;C`;
       feel.innerHTML = `Feels Like: ${data.main.feels_like}&deg;C`;
